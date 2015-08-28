1eb2fe949e9148ea67c8e2de9fb1ab55bec565c7
Wolfgang Wiedermann <wolfgang.wiedermann@hs-regensburg.de>
2015-08-28 18:36:05 +0200

    added support for lists als return types at get requests for java client generator

--- src/generator/java_client.rs
+++ src/generator/java_client.rs
@@ -127,7 +127,7 @@ fn gen_proxy(ifa:&Box<model::Interface>, types:Box<Vec<Box<model::Type>>>) -> St
     str.push_str(&ifa.get_param_value("java-package"));
     str.push_str(";");
 } 
-  str.push_str("\n\nimport java.util.ArrayList; \nimport org.apache.http.HttpEntity;\nimport org.apache.http.HttpResponse;\nimport org.apache.http.client.HttpClient;\nimport org.apache.http.client.methods.HttpGet;\nimport org.apache.http.impl.client.HttpClients;\nimport com.fasterxml.jackson.databind.ObjectMapper;");
+  str.push_str("\n\nimport java.util.ArrayList; \nimport java.util.Arrays; // TODO: pruefen ob function mit returntype_is_array \nimport org.apache.http.HttpEntity;\nimport org.apache.http.HttpResponse;\nimport org.apache.http.client.HttpClient;\nimport org.apache.http.client.methods.HttpGet;\nimport org.apache.http.impl.client.HttpClients;\nimport com.fasterxml.jackson.databind.ObjectMapper;");
   str.push_str(&get_proxies_referenced_java_packages(&ifa, types.clone()));
   str.push_str("\n\n/**\n* Generated Proxy for ");
   str.push_str(&ifa.name);
@@ -203,9 +203,23 @@ for param in f.params.iter() {
       str.push_str(");");
     }
 } 
-  str.push_str("\n\t    \tHttpGet get = new HttpGet(path);\n\t\t\tHttpResponse resp;\n\t\t\tresp = clnt.execute(get);\n\t\t\tHttpEntity httpEntity = resp.getEntity();\n\t\t\treturn mapper.readValue(httpEntity.getContent(), ");
-  str.push_str(&get_java_type(&f.returntype, f.returntype_is_array));
-  str.push_str(".class);\n    \t} catch(Exception ex) {\n    \t\tthrow new RuntimeException(ex);\n    \t}");
+  str.push_str("\n\t    \tHttpGet get = new HttpGet(path);\n\t\t\tHttpResponse resp;\n\t\t\tresp = clnt.execute(get);\n\t\t\tHttpEntity httpEntity = resp.getEntity();");
+if f.returntype_is_array { 
+    str.push_str("\n            ");
+    str.push_str(&get_java_type(&f.returntype, f.returntype_is_array));
+    str.push_str(" lst = new ");
+    str.push_str(&get_java_type(&f.returntype, f.returntype_is_array));
+    str.push_str("();\n\t\t\t");
+    str.push_str(&get_java_type(&f.returntype, false));
+    str.push_str("[] array = mapper.readValue(httpEntity.getContent(), ");
+    str.push_str(&get_java_type(&f.returntype, false));
+    str.push_str("[].class);\n\t\t\tlst.addAll(Arrays.asList(array));\n\t\t\treturn lst;");
+} else { 
+    str.push_str("\n\t\t\treturn mapper.readValue(httpEntity.getContent(), ");
+    str.push_str(&get_java_type(&f.returntype, f.returntype_is_array));
+    str.push_str(".class);");
+} 
+  str.push_str("\n    \t} catch(Exception ex) {\n    \t\tthrow new RuntimeException(ex);\n    \t}");
 
   return str;
 }
