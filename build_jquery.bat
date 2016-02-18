827304e92ee69ac73d6595626d76b93440fc7727
wolfgang-wiedermann <wolfgang.wiedermann@hs-regensburg.de>
2016-02-18 16:44:09 +0100

    build script for jquery proxy generator completed

--- build_jquery.bat
+++ build_jquery.bat
@@ -1,2 +1,3 @@
 python tools/generate_code.py  ./templates/jquery_template.ct c > ./src/generator/jquery.rs
-
+cargo build
+./target/debug/staticjson ./target/debug/test_studenten.sjs -t jquery --debug
